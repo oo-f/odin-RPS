b3c41caa298037ba401e189f93d46bff9728dd38
oo-f <179203317+oo-f@users.noreply.github.com>
2024-10-01 16:51:14 -0700

    Finish logic for 5 round loop and score keeping
    Also cleaned up some code

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 
 
-
+//Generates and assigns random number to string
 const getComputerChoice = () => 
   Math.random() <= 0.3 ? "rock" :
   Math.random() <= 0.6 ? "paper" : "scissors"
 
-
-
+//Prompts player to type 
 const getHumanChoice = () =>
   prompt("Rock, Paper, or Scissors");
 
-// const humanScore = 0;
-// const computerScore = 0;
+let  humanScore = 0;
+let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) =>
-humanChoice === computerChoice ? "It's a Tie!": 
-humanChoice === "rock" && computerChoice === "paper" ||
-humanChoice === "paper" && computerChoice === "scissors" ||
-humanChoice === "scissors" && computerChoice === "rock" ? "Computer Wins!!" :
-"You Win!";
+//Logic 
+  const playRound = (humanChoice, computerChoice) =>
+    humanChoice === computerChoice ? "It's a Tie!": 
+    humanChoice === "rock" && computerChoice === "paper" ||
+    humanChoice === "paper" && computerChoice === "scissors" ||
+    humanChoice === "scissors" && computerChoice === "rock" ? 
+    "Computer Wins!" : "You Win!";
 
+//Loop to play 5 rounds
+    for (let i = 0; i < 5; i++) {
+      const humanChoice = getHumanChoice().slice(0).toLowerCase();
+      const computerChoice = getComputerChoice();
 
-const humanChoice = getHumanChoice().slice(0).toLowerCase();
-const computerChoice = getComputerChoice();
-console.log(playRound(humanChoice, computerChoice));
+const results = playRound(humanChoice, computerChoice)
+
+console.log(`Round: ${i + 1}              ${results}`);
 console.log(`Your Move: ${humanChoice}`);
 console.log(`Computer Move: ${computerChoice}`);
+
+//Adds 1 to respective score based on outcome
+    results === "You Win!" ? humanScore++ :
+    results === "Computer Wins!" ? computerScore++ : null;
+
+console.log(`Computer Points: ${computerScore}   Your Points: ${humanScore}`);
+}
